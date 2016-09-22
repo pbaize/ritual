6ec77f934dff29a974d7d56f3ac5eec9d06ed50b
Pierre Baize <pierrebaize@Pierres-MacBook-Pro.local>
2016-09-22 12:12:11 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const app = require('koa')()
+const router = require('koa-router')()
+const json = require('koa-json')
+// const views = require('koa-views')
+const onerror = require('koa-onerror')
+const serve = require('koa-static')
+const path = require('path')
+const log4js = require('koa-log4')
+const logger = log4js.getLogger('app')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// middlewares
+app.use(require('koa-bodyparser')())
+app.use(json())
+app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }))
+app.use(serve(path.join(__dirname, 'public')))
+
+// setup view
+app.use(serve(path.join(__dirname, 'views')))
+
+// handle error
+onerror(app)
+
+// logger
+app.use(function * (next) {
+  var start = new Date()
+  yield next
+  var ms = new Date() - start
+  logger.info('%s %s - %s', this.method, this.url, ms)
+})
+
+// routes definition
+router.use('/', index.routes(), index.allowedMethods())
+router.use('/users', users.routes(), users.allowedMethods())
+
+// mount root routes
+app.use(router.routes())
+  .use(router.allowedMethods())
+
+// log error
+app.on('error', function (err, ctx) {
+  logger.error('server error', err, ctx)
+})
+
+module.exports = app
